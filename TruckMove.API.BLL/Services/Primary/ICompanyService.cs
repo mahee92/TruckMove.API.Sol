be29b85a4e89d0b9598a571f889c407121923ad5
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-05-15 00:03:12 +0530

    TRUK-5 | API-Design

---
+++ TruckMove.API.BLL/Services/Primary/ICompanyService.cs
@@ -0,0 +1,9 @@
+﻿using TruckMove.API.BLL.Models.Primary;
+
+namespace TruckMove.API.BLL.Services.Primary
+{
+    public interface ICompanyService
+    {
+        IEnumerable<Company> Get();
+    }
+}
---
+++ TruckMove.API.DAL/Repositories/Primary/CompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿
+using TruckMove.API.DAL.Models;
+
+namespace TruckMove.API.DAL.Repositories.Primary
+{
+    public class CompanyDataRepository : ICompanyDataRepository
+    {
+        private readonly TrukMoveLocalContext _context;
+        public CompanyDataRepository(TrukMoveLocalContext context)
+        {
+            _context = context;
+        }
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+         };
+        public IEnumerable<CompanyModel> Get()
+        {
+            var xx = _context.Companies.ToList();
+            return xx;
+        }
+
+
+    }
+}
