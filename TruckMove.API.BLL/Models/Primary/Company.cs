c77b2d0626b0c13081ca371efe5bff15ed5988cf
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-05-15 00:03:12 +0530

    TRUK-5 | API-Design

---
+++ TruckMove.API.BLL/Models/Primary/Company.cs
@@ -0,0 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TruckMove.API.BLL.Models.Primary
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ TruckMove.API.DAL/Repositories/Primary/ICompanyRepository.cs
@@ -0,0 +1,10 @@
+﻿using TruckMove.API.DAL.DTO;
+using TruckMove.API.DAL.Models;
+
+namespace TruckMove.API.DAL.Repositories.Primary
+{
+    public interface ICompanyDataRepository
+    {
+        IEnumerable<CompanyModel> Get();
+    }
+}
