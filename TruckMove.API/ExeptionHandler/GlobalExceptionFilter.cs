8e0438647e0c5c10f8a9dc10d71ec031c9585a24
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-05-16 13:02:41 +0530

    TRUK-5 | API-Design - Error handler

---
+++ TruckMove.API/ExeptionHandler/GlobalExceptionFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TruckMove.API.ExeptionHandler
+{
+    public class GlobalExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            var statusCode = context.Exception switch
+            {
+                NotFoundException => StatusCodes.Status404NotFound,
+
+                ValidationException => StatusCodes.Status400BadRequest,
+
+                UnauthorizedAccessException => StatusCodes.Status401Unauthorized,
+
+                _ => StatusCodes.Status500InternalServerError
+            };
+
+            context.Result = new ObjectResult(new
+            {
+                error = context.Exception.Message,
+                stackTrace = context.Exception.StackTrace
+            })
+            {
+                StatusCode = statusCode
+            };
+
+           
+        }
+
+    
+    }
+ 
+}
--- TruckMove.API/Program.cs
+++ TruckMove.API/Program.cs
@@ -1,37 +1,48 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using System.Data.Common;
-using TruckMove.API.BLL.Services;
+using TruckMove.API.BLL.Services.Primary;
 using TruckMove.API.DAL;
 using TruckMove.API.DAL.Models;
-using TruckMove.API.DAL.Repositories;
+using TruckMove.API.DAL.Repositories.Primary;
+using TruckMove.API.ExeptionHandler;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddTransient<ICompanyService, CompanyService>();   
-builder.Services.AddTransient<ICompanyDataRepository, CompanyDataRepository>(); 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<TrukMoveLocalContext>(option =>
-    option.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabaseConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers(options =>
+        {
+            options.Filters.Add(new GlobalExceptionFilter());
+        });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddTransient<ICompanyService, CompanyService>();
+        builder.Services.AddTransient<ICompanyDataRepository, CompanyDataRepository>();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+        builder.Services.AddDbContext<TrukMoveLocalContext>(option =>
+            option.UseSqlServer(builder.Configuration.GetConnectionString("MyDatabaseConnection")));
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
