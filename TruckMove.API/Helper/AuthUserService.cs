b11d3afa387cc6a7d23af80330f3fb1aefa01ee8
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-06-22 19:45:52 +0530

    mobile

--- TruckMove.API/Helper/AuthUserService.cs
+++ TruckMove.API/Helper/AuthUserService.cs
@@ -18,6 +18,7 @@ namespace TruckMove.API.Helper
 #if DEBUG
             // This code will only run in debug mode
             return "2";
+           // return "21";
 #else
             return _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
 #endif
