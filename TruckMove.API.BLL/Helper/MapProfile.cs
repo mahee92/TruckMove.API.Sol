418c3cc584f655cfa3b5f3cfe9445828196be7eb
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-08-12 14:08:24 +0530

    --

--- TruckMove.API.BLL/Helper/MapProfile.cs
+++ TruckMove.API.BLL/Helper/MapProfile.cs
@@ -33,7 +33,7 @@ namespace TruckMove.API.BLL.Helper
             CreateMap<Vehicle, VehicleDto>();
             CreateMap<VehicleOutputDto, Vehicle>();
             CreateMap<Vehicle, VehicleOutputDto>()
-                .ForMember(dest => dest.Notes, opt => opt.MapFrom(src => src.Notes)
+                .ForMember(dest => dest.Notes, opt => opt.MapFrom(src => src.Notes))
                 .ForMember(dest => dest.Images, opt => opt.MapFrom(src => src.Images)
             );
 
