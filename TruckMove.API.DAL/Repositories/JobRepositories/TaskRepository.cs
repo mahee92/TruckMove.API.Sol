6a66500b231ef38de618eb122a77018b73121672
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-08-13 17:06:52 +0530

    --

--- TruckMove.API.DAL/Repositories/JobRepositories/TaskRepository.cs
+++ TruckMove.API.DAL/Repositories/JobRepositories/TaskRepository.cs
@@ -16,24 +16,45 @@ namespace TruckMove.API.DAL.Repositories.JobRepositories
 
         public async Task<List<Accommodation>> GetAccommodationTasksByUserId(int userId)
         {
-            return await _context.Set<Accommodation>().Where(x => x.OrganizeNow == false && x.Assignee == userId && x.Status != (int)TaskStatusEnum.Completed && x.IsActive==true).ToListAsync();
+            return await _context.Set<Accommodation>().Where(x => x.Job.IsActive == true &&
+                                                                   x.Job.Status < (int)JobStatusEnum.QADone &&
+                                                                   x.OrganizeNow == false &&
+                                                                   x.Assignee == userId &&
+                                                                   x.Status != (int)TaskStatusEnum.Completed &&
+                                                                   x.IsActive == true).ToListAsync();
         }
         public async Task<List<PermitsAndPlate>> GetPermitsAndPlateTasksByUserId(int userId)
         {
-            return await _context.Set<PermitsAndPlate>().Where(x => x.OrganizeNow == false && x.Assignee == userId && x.Status != (int)TaskStatusEnum.Completed && x.IsActive == true).ToListAsync();
+            return await _context.Set<PermitsAndPlate>().Where(x =>x.Job.IsActive==true && 
+                                                                   x.Job.Status < (int)JobStatusEnum.QADone &&
+                                                                   x.OrganizeNow == false && 
+                                                                   x.Assignee == userId && 
+                                                                   x.Status != (int)TaskStatusEnum.Completed &&
+                                                                   x.IsActive == true).ToListAsync();
         }
         public async Task<List<PublicTransport>> GetPublicTransportTasksByUserId(int userId)
         {
-            return await _context.Set<PublicTransport>().Where(x => x.OrganizeNow == false && x.Assignee == userId && x.Status != (int)TaskStatusEnum.Completed && x.IsActive == true).ToListAsync();
+            return await _context.Set<PublicTransport>().Where(x => x.Job.IsActive == true &&
+                                                                   x.Job.Status < (int)JobStatusEnum.QADone &&
+                                                                   x.OrganizeNow == false &&
+                                                                   x.Assignee == userId &&
+                                                                   x.Status != (int)TaskStatusEnum.Completed &&
+                                                                   x.IsActive == true).ToListAsync();
         }
         public async Task<List<Purchase>> GetPurchaseTasksByUserId(int userId)
         {
-            return await _context.Set<Purchase>().Where(x => x.OrganizeNow == false && x.Assignee == userId && x.Status != (int)TaskStatusEnum.Completed && x.IsActive == true).ToListAsync();
+            return await _context.Set<Purchase>().Where(x => x.Job.IsActive == true &&
+                                                                   x.Job.Status < (int)JobStatusEnum.QADone &&
+                                                                   x.OrganizeNow == false &&
+                                                                   x.Assignee == userId &&
+                                                                   x.Status != (int)TaskStatusEnum.Completed &&
+                                                                   x.IsActive == true).ToListAsync();
         }
         public async Task<List<Job>> GetJobTasksByUserId(int userId)
         {
-            return await _context.Set<Job>().Where(x =>  x.IsActive == true
-                                                         && x.Controller== userId).ToListAsync();
+            return await _context.Set<Job>().Where(x =>  x.IsActive == true &&
+                                                         x.Controller== userId &&
+                                                         x.Status != (int)JobStatusEnum.Completed).ToListAsync();
 
             //x.Status == (int)JobStatusEnum.ArrivalChecked ||
             //x.Status == (int)JobStatusEnum.QADone || 
