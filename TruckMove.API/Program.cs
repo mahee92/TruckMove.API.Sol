ed5a77f7360e8a87b18cc0cbc0df75cc6965c48e
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-08-24 10:17:50 +0530

    --

--- TruckMove.API/Program.cs
+++ TruckMove.API/Program.cs
@@ -28,8 +28,8 @@ using Microsoft.AspNetCore.OData;
 using Microsoft.OData.ModelBuilder;
 using Microsoft.OData.Edm; // Added for OData
 using TruckMove.API.BLL.Models.TaskDTOs;
-using Microsoft.OpenApi.Any;
-using static TruckMove.API.DAL.MasterData.MasterData;
+//using Microsoft.OpenApi.Any;
+//using static TruckMove.API.DAL.MasterData.MasterData;
 //using Newtonsoft.Json.Serialization;
 
 internal class Program
@@ -218,23 +218,23 @@ internal class Program
         {
             c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 
-            c.MapType<Dictionary<JobStatusEnum, bool>>(() => new OpenApiSchema
-            {
-                Type = "object",
-                AdditionalProperties = new OpenApiSchema
-                {
-                    Type = "boolean"
-                },
-                Properties = Enum.GetValues(typeof(JobStatusEnum))
-          .Cast<JobStatusEnum>()
-          .ToDictionary(
-              status => status.ToString(),
-              status => new OpenApiSchema
-              {
-                  Type = "boolean",
-                  Example = new OpenApiBoolean(false) // Set default value
-              })
-            });
+          //  c.MapType<Dictionary<JobStatusEnum, bool>>(() => new OpenApiSchema
+          //  {
+          //      Type = "object",
+          //      AdditionalProperties = new OpenApiSchema
+          //      {
+          //          Type = "boolean"
+          //      },
+          //      Properties = Enum.GetValues(typeof(JobStatusEnum))
+          //.Cast<JobStatusEnum>()
+          //.ToDictionary(
+          //    status => status.ToString(),
+          //    status => new OpenApiSchema
+          //    {
+          //        Type = "boolean",
+          //        Example = new OpenApiBoolean(false) // Set default value
+          //    })
+          //  });
 
             // Add JWT Authentication
             var securityScheme = new OpenApiSecurityScheme
