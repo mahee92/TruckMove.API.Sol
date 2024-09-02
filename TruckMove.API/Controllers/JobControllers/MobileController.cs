b37663d45549b5ff928cfbc2c0805d506288cac4
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-09-02 13:05:53 +0530

    --

--- TruckMove.API/Controllers/JobControllers/MobileController.cs
+++ TruckMove.API/Controllers/JobControllers/MobileController.cs
@@ -10,6 +10,7 @@ using TruckMove.API.BLL.Models.TaskDTOs;
 using TruckMove.API.BLL.Services.JobServices;
 using TruckMove.API.Helper;
 using TruckMove.API.Settings;
+using static TruckMove.API.DAL.MasterData.MasterData;
 
 namespace TruckMove.API.Controllers.JobControllers
 {
@@ -125,6 +126,37 @@ namespace TruckMove.API.Controllers.JobControllers
             else
             {
 
+                return StatusCode((int)response.ErrorType, response.ErrorMessage);
+            }
+        }
+       
+        [HttpPost("UpdateStatus")]
+        public async Task<IActionResult> UpdateStatus(int jobId, bool QA_Done = false, bool delayOccurred = false, bool InProgress = false, bool Stoped = false)
+        {
+            Response response = new Response();
+            if (QA_Done)
+            {
+                response = await _jobService.UpdateStatus(jobId, JobStatusEnum.QADone);
+            }
+            else if (delayOccurred)
+            {
+                response = await _jobService.UpdateStatus(jobId, JobStatusEnum.Delayed);
+            }
+            else if (InProgress)
+            {
+                response = await _jobService.UpdateStatus(jobId, JobStatusEnum.InProgress);
+            }
+            else if (Stoped)
+            {
+                response = await _jobService.UpdateStatus(jobId, JobStatusEnum.Stopped);
+            }
+
+            if (response.Success)
+            {
+                return Ok(response.data);
+            }
+            else
+            {
                 return StatusCode((int)response.ErrorType, response.ErrorMessage);
             }
         }
