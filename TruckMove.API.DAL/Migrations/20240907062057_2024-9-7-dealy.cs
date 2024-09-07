b1f812de0f5702b1f2e30af1557f01ead5db9083
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-09-07 21:43:36 +0530

    --

---
+++ TruckMove.API.DAL/Migrations/20240907062057_2024-9-7-dealy.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TruckMove.API.DAL.Migrations
+{
+    public partial class _202497dealy : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Delays",
+                table: "Notes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Delays",
+                table: "Notes",
+                column: "DelayId",
+                principalTable: "Delays",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Notes_Delays",
+                table: "Notes");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Notes_Delays",
+                table: "Notes",
+                column: "DelayId",
+                principalTable: "Delays",
+                principalColumn: "Id");
+        }
+    }
+}
