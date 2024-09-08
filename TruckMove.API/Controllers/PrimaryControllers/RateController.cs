6a19fd5a5c6dc3640185d916c30d2c32a280acc3
mahee92 <155177933+mahee92@users.noreply.github.com>
2024-09-08 12:28:51 +0530

    --

--- TruckMove.API/Controllers/PrimaryControllers/RateController.cs
+++ TruckMove.API/Controllers/PrimaryControllers/RateController.cs
@@ -1,47 +1,47 @@
-﻿//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Options;
-//using TruckMove.API.BLL.Helper;
-//using TruckMove.API.BLL.Models.PrimaryDTOs;
-//using TruckMove.API.BLL.Services.PrimaryServices;
-//using TruckMove.API.Helper;
-//using TruckMove.API.Settings;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TruckMove.API.BLL.Helper;
+using TruckMove.API.BLL.Models.PrimaryDTOs;
+using TruckMove.API.BLL.Services.PrimaryServices;
+using TruckMove.API.Helper;
+using TruckMove.API.Settings;
 
-//namespace TruckMove.API.Controllers.PrimaryControllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//#if DEBUG
-//#else
-//     [Authorize(Roles = "Administrator)]
-//#endif
-//    public class RateController : ControllerBase
-//    {
-//        private readonly ILogger<RateController> _logger;
-//        private readonly IRateService _rateService;
-//        private readonly MySettings _mySettings;
-//        private readonly IAuthUserService _authUserService;
+namespace TruckMove.API.Controllers.PrimaryControllers
+{
+    [ApiController]
+    [Route("[controller]")]
+#if DEBUG
+#else
+     [Authorize(Roles = "Administrator)]
+#endif
+    public class RateController : ControllerBase
+    {
+        private readonly ILogger<RateController> _logger;
+        private readonly IRateService _rateService;
+        private readonly MySettings _mySettings;
+        private readonly IAuthUserService _authUserService;
 
-//        public RateController(ILogger<RateController> logger, IRateService rateService, IOptions<MySettings> mySettings, IAuthUserService authUserService)
-//        {
-//            _logger = logger;
-//            _rateService = rateService;
-//            _mySettings = mySettings.Value;
-//            _authUserService = authUserService;
-//        }
-//        [HttpPut("Update")]
-//        public async Task<IActionResult> Update([FromBody] UpdateRateValueDto updateRateValueDto)
-//        {
-//           var response = await _rateService.UpdateRateValueAsync(updateRateValueDto,1);
-//            if (response.Success)
-//            {
-//                return Ok(response.Object);
-//            }
-//            else
-//            {
-//                return StatusCode((int)response.ErrorType, response.ErrorMessage);
-//            }
-//        }
+        public RateController(ILogger<RateController> logger, IRateService rateService, IOptions<MySettings> mySettings, IAuthUserService authUserService)
+        {
+            _logger = logger;
+            _rateService = rateService;
+            _mySettings = mySettings.Value;
+            _authUserService = authUserService;
+        }
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update([FromBody] UpdateRateValueDto updateRateValueDto)
+        {
+            var response = await _rateService.UpdateRateValueAsync(updateRateValueDto, 1);
+            if (response.Success)
+            {
+                return Ok(response.Object);
+            }
+            else
+            {
+                return StatusCode((int)response.ErrorType, response.ErrorMessage);
+            }
+        }
 
-//    }
-//}
+    }
+}
